9a9f65dd9b71585f555d7e6e4d0d59793f8a9072
hara <sw135_@naver.com>
2023-10-26 15:30:02 +0900

    second commit

--- 나라선택/java/CustomAdapter.java
+++ 나라선택/java/CustomAdapter.java
@@ -3,51 +3,101 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.ArrayList;
+import java.util.List;
 
-public class CustomAdapter extends ArrayAdapter<country_name> {
+public class CustomAdapter extends ArrayAdapter<country_name> implements Filterable {
     private Context context;
     private int resource;
-    private ArrayList<country_name> countries;
+    private List<country_name> originalCountries; // 원본 데이터
+    private List<country_name> filteredCountries; // 필터링된 데이터
+    private Filter countryFilter;
 
-    // 생성자: 커스텀 어댑터를 초기화
     public CustomAdapter(Context context, int resource, ArrayList<country_name> countries) {
         super(context, resource, countries);
         this.context = context;
         this.resource = resource;
-        this.countries = countries;
+        this.originalCountries = new ArrayList<>(countries);
+        this.filteredCountries = new ArrayList<>(countries);
     }
 
-    // 각 항목의 뷰를 생성하고 설정
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder;
 
         if (convertView == null) {
-            // 레이아웃 인플레이션
+            // 리스트 뷰 아이템을 인플레이트하고 뷰 홀더를 설정
             LayoutInflater inflater = LayoutInflater.from(context);
             convertView = inflater.inflate(resource, parent, false);
 
             holder = new ViewHolder();
-            holder.flag = convertView.findViewById(R.id.flag); // 이미지뷰
-            holder.name = convertView.findViewById(R.id.name); // 텍스트뷰
+            holder.flag = convertView.findViewById(R.id.flag);
+            holder.name = convertView.findViewById(R.id.name);
 
             convertView.setTag(holder);
         } else {
+            // 이미 인플레이트된 아이템의 뷰 홀더를 재사용
             holder = (ViewHolder) convertView.getTag();
         }
 
-        country_name country = countries.get(position);
-        // 이미지뷰와 텍스트뷰에 데이터 설정
+        // 현재 위치의 나라 정보를 가져와서 뷰에 설정
+        country_name country = getItem(position);
         holder.flag.setImageResource(country.getFlagResource());
         holder.name.setText(country.getName());
 
         return convertView;
     }
 
-    // 뷰 홀더 패턴을 사용하여 뷰 홀더 클래스 정의
+    @Override
+    public Filter getFilter() {
+        // 필터를 반환하며, 필터가 없는 경우 새로 생성
+        if (countryFilter == null) {
+            countryFilter = new CountryFilter();
+        }
+        return countryFilter;
+    }
+
+    private class CountryFilter extends Filter {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults();
+
+            if (constraint == null || constraint.length() == 0) {
+                // 검색어가 없는 경우, 원본 데이터를 그대로 사용
+                results.values = new ArrayList<>(originalCountries);
+                results.count = originalCountries.size();
+            } else {
+                String query = constraint.toString().toLowerCase();
+                List<country_name> filtered = new ArrayList<>();
+
+                for (country_name country : originalCountries) {
+                    // 검색어가 포함된 나라만 필터링
+                    if (country.getName().toLowerCase().contains(query)) {
+                        filtered.add(country);
+                    }
+                }
+
+                results.values = filtered;
+                results.count = filtered.size();
+            }
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            // 필터링된 결과를 리스트뷰에 반영
+            filteredCountries = (List<country_name>) results.values;
+            clear();
+            addAll(filteredCountries);
+            notifyDataSetChanged();
+        }
+    }
+
     static class ViewHolder {
         ImageView flag;
         TextView name;
--- 나라선택/java/mainActivity.java
+++ 나라선택/java/mainActivity.java
@@ -2,11 +2,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.SearchView;
+
 import java.util.ArrayList;
 
 public class mainActivity extends AppCompatActivity {
 
     ListView listView;
+    CustomAdapter adapter;
+    ArrayList<country_name> countries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,22 +26,37 @@ public class mainActivity extends AppCompatActivity {
         countries.add(new country_name("베트남", R.drawable.america));
         countries.add(new country_name("대만", R.drawable.america));
         countries.add(new country_name("중국", R.drawable.america));
-        countries.add(new country_name("영인이", R.drawable.america));
-        countries.add(new country_name("영인이네 나라", R.drawable.america));
-        countries.add(new country_name("재준이나라", R.drawable.america));
-        countries.add(new country_name("재준국", R.drawable.america));
-        countries.add(new country_name("전재준국", R.drawable.america));
-        countries.add(new country_name("영인재준나라", R.drawable.america));
+        countries.add(new country_name("나라1", R.drawable.america));
+        countries.add(new country_name("나라2", R.drawable.america));
+        countries.add(new country_name("나라3", R.drawable.america));
+        countries.add(new country_name("나라4", R.drawable.america));
+        countries.add(new country_name("나라5", R.drawable.america));
+        countries.add(new country_name("나라6", R.drawable.america));
 
         // 다른 국가 데이터 추가
 
         CustomAdapter adapter = new CustomAdapter(this, R.layout.list_item, countries);
         listView.setAdapter(adapter);
 
-    }
-    
-    public void onBakButtonClick(View view) {
-        onBackPressed(); // 뒤로가기 동작 실행
+        // SearchView 설정
+        SearchView searchView = findViewById(R.id.serch);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // 사용자가 쿼리를 제출할 때 동작을 수행할 수 있습니다.
+                return true;
+            }
 
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // 사용자가 입력할 때 ListView 데이터를 필터링합니다.
+                adapter.getFilter().filter(newText);
+                return true;
+            }
+        });
+    }
+
+    public void onBakButtonClick(View view) {
+        onBackPressed(); // 뒤로 가기
     }
 }
